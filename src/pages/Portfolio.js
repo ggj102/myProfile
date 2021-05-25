732dc41b9ee2529d318bd62be749150dd1962596
Kwangjin Kim <ggj101@naver.com>
2021-05-25 23:41:11 +0900

    변경점 수정완료 - useEffect array 추가 - height 고정값으로 변경 - styled-component 분리

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,4 +1,3 @@
-import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFileAlt } from "@fortawesome/free-solid-svg-icons";
 import { faHome } from "@fortawesome/free-solid-svg-icons";
@@ -6,155 +5,8 @@ import { faGithubSquare } from "@fortawesome/free-brands-svg-icons"
 import { useSelector } from 'react-redux';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import  Contents from '../styled_components/portfolioStyle';
 
-
-const Content = styled.div`
-    a:link{text-decoration: none; color:${(props)=> props.color};}
-    a:visited{text-decoration: none; color:${(props)=> props.color};}
-    a:active{text-decoration: none; color:${(props)=> props.color};}
-    a:hover{text-decoration: none; color:${(props)=> props.color};}
-    .portfolio{
-        padding: 150px 85px 0 85px;
-        margin: 0 auto;
-        max-width: 1140px;
-        box-sizing: border-box;
-        color: #fff;
-        font-size: 16px;
-    }
-    ul{
-        list-style: none;
-        padding: 0;
-        margin: 0;
-    }
-    .item{
-        width: 49%;
-        margin-bottom: 30px;
-        box-sizing: border-box;
-        padding: 10px;
-        background-color: ${(props)=>props.background};
-        border-radius:10px;
-    }
-    .left{
-        float: left;
-    }
-    .right{
-        float: right;
-    }
-    .item img{
-        width: 100%;
-        height: 194px;
-        border-radius:10px;
-    }
-    .item::after{
-        display: block;
-        clear:both;
-        content: '';
-    }
-    .link_area{
-        display:flex;
-        justify-content: space-between;
-        margin-top: 15px;
-    }
-    .icon_area{
-        display:flex;
-        color:${(props)=>props.color};
-    }
-    .pf_title{
-        font-size: 25px;
-        font-weight: bold;
-        color:${(props)=>props.color};
-    }
-    .icon_center{
-        margin: 0 10px;
-    }
-    @media screen and (min-width: 481px) and (max-width: 1024px)
-    {
-        .portfolio{
-            padding: 150px 25px 0 25px;
-            margin: 0 auto;
-            max-width: 1140px;
-            box-sizing: border-box;
-            color: #fff;
-            font-size: 16px;
-        }
-    }
-    @media screen and (min-width: 481px) and (max-width: 850px)
-    {
-        .portfolio{
-            padding: 0 25px;
-            margin: 0 auto;
-            max-width: 1140px;
-            box-sizing: border-box;
-            color: #fff;
-            font-size: 16px;
-        }
-        .left{
-            float: none;
-        }
-        .right{
-            float: none;
-        }
-        .item{
-            width: 100%;
-            margin-bottom: 30px;
-            box-sizing: border-box;
-            padding: 10px;
-            border-radius:10px;
-        }
-        .item img{
-            width: 100%;
-            height: auto;
-            border-radius:10px;
-        }
-    }
-    @media screen and (max-width: 480px)
-    {
-        .portfolio{
-            padding: 0 25px;
-            margin: 0 auto;
-            max-width: 1140px;
-            box-sizing: border-box;
-            color: #fff;
-            font-size: 16px;
-        }
-        .left{
-            float: none;
-        }
-        .right{
-            float: none;
-        }
-        .item{
-            width: 100%;
-            
-            margin-bottom: 30px;
-            box-sizing: border-box;
-            padding: 10px;
-            border-radius:10px;
-        }
-        .item img{
-            width: 100%;
-            height: auto;
-            border-radius:10px;
-        }
-        .pf_title{
-            text-align: center;
-            font-size: 25px;
-            font-weight: bold;
-        }
-    }
-    @media screen and (max-width: 450px){
-        .link_area{
-            display:flex;
-            flex-direction:column;
-            margin-top: 10px;
-        }
-        .icon_area{
-            display:flex;
-            margin-top: 15px;
-            justify-content: center;
-        }
-    }
-`
 
 function Portfolio(){
     const color = useSelector(state => state.color);
@@ -163,7 +15,7 @@ function Portfolio(){
         axios.get("/address.json").then((response)=>{
             setAddressData(response.data);
         })
-    });
+    },[]);
     
     const pfList = addressData.map((val,idx)=>{
         return(
@@ -194,13 +46,13 @@ function Portfolio(){
     })
 
     return(
-        <Content background={color.bg2} color={color.font}>
+        <Contents background={color.bg2} color={color.font}>
             <div className="portfolio">
                 <ul>
                     {pfList}
                 </ul>
             </div>
-        </Content>
+        </Contents>
     )
 }
 
