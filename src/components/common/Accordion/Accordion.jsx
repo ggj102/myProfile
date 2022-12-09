766098158007676128e8ce5cf87bae56683fa72d
ggj102 <ggj101.naver.com>
2022-12-09 14:09:59 +0900

    아코디언 컴포넌트 추가

---
+++ src/components/common/Accordion/Accordion.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
+
+export default function Accordion({ title, children }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const onClickAccordion = () => setIsOpen(!isOpen);
+
+  return (
+    <>
+      <button className="accordion_btn" onClick={onClickAccordion}>
+        <span className="point_text">{title}</span>
+        <FontAwesomeIcon icon="fa-triangle" size="2x" />
+      </button>
+      <div className={isOpen ? "" : "display_none"}>{children}</div>
+    </>
+  );
+}
